90d53c15684970a9e6d0b93c1e27f61bdda63347
logan-brown8520 <32582649+logan-brown8520@users.noreply.github.com>
2018-11-16 15:37:49 -0600

    starter code for lab 3

---
+++ Lab3/Lab3.ino
@@ -0,0 +1,14 @@
+void home_page(){
+  bmpDraw("home_page.bmp", x, y);
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  home_page()
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
